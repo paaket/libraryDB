2f9d0eb9aea40a7629a262b8d3fdba36ec4c9494
paaket <122210350+paaket@users.noreply.github.com>
2026-06-10 14:18:24 +0300

    feat: add book

--- library/Menu.cpp
+++ library/Menu.cpp
@@ -4,11 +4,12 @@ void Menu::start(const DataBase& dataBase) {
 	bool flag = true;
 	while (flag) {
 		int option;
-		std::cout << "\nMAIN MENU:\n1 - print data base\n2 - exit\noption: ";
+		std::cout << "\nMAIN MENU:\n1 - print data base\n2 - add book\n3 - exit\noption: ";
 		std::cin >> option;
 		switch (option) {
 		case 1: dataBase.printDataBase(); break;
-		case 2: flag = false; break;
+		case 2: dataBase.addBook(); break;
+		case 3: flag = false; break;
 		}
 	}
 }
