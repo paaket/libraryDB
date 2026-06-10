3d9ae22197320d233b9721387d07b9e89b5ef0fd
paaket <122210350+paaket@users.noreply.github.com>
2026-06-10 13:33:24 +0300

    feat: add printDataBase()

--- library/DataBase.h
+++ library/DataBase.h
@@ -7,22 +7,9 @@
 
 class DataBase {
 public:
-    struct Book {
-        std::string bookName;
-        std::string authorFirst;
-        std::string authorLast;
-        int year;
-        std::string genre;
-        int pagesCount;
-        double rating;
-    };
     DataBase(const std::string& fileName);
     ~DataBase();
-    void getDataBase();
-    void addBook(const Book& book);
-    void printBookInfo(const Book& book);
-    void printDataBase();
+    void printDataBase() const;
 private:
     sqlite3* dataBase = nullptr;
-    std::vector<Book> enteredDataBase;
 };
